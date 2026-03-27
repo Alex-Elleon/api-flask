7f5413d59d822bc7f666f5283993595816d6f2ba
Alex-Elleon <naric@Alexelleon.com>
2026-03-27 15:06:42 -0600

    Variables de entorno y requerimientos

--- db.py
+++ db.py
@@ -1,13 +1,18 @@
 import pyodbc
+import os
 
 def get_connection():
-    conn = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=Consultorio1.mssql.somee.com,1433;"
-        "DATABASE=Consultorio1;"
-        "UID=EdwinA180605_SQLLogin_2;"
-        "PWD=vrnv83posc;"
-        "Encrypt=yes;"
-        "TrustServerCertificate=yes;"
-    )
-    return conn
+    driver = "{ODBC Driver 17 for SQL Server}"
+    server = os.environ.get('DB_SERVER')
+    database = os.environ.get('DB_NAME')
+    username = os.environ.get('DB_USER')
+    password = os.environ.get('DB_PASSWORD')
+
+    connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    
+    try:
+        conn = pyodbc.connect(connection_string)
+        return conn
+    except Exception as e:
+        print(f"ERROR DE CONEXIÓN A SOMEE: {e}")
+        raise e
